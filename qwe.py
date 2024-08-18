733ea978602f056208eb7054a5688e6646a8ba8a
ISCUSSTVO <nikishka228@list.ru>
2024-08-18 10:36:34 +1000

    хуй

--- qwe.py
+++ qwe.py
@@ -3,20 +3,26 @@ import os
 
 from aiogram import Bot, Dispatcher
 from handlers.user import user_router
+from handlers.admin import admin_router
+
 
 from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
 
 
-ALUPD = ['message', 'callbackquery', 'edited_message']
+
 bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher()
 
 
-dp.include_routers(user_router)
+dp.include_routers(user_router, admin_router)
+
+
+async def on_startup(dp):
+    print("Бот запущен и готов к работе!")
 
 async def main():
-    bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot, allowed_updates=ALUPD)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 asyncio.run(main())
